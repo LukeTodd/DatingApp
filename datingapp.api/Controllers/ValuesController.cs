b8fa1b58b7dca8f917f07d85a105730ab64e7b81
Luke Todd <luketodd@Luke-Todd--luketoddhappyhippoherbalscom--MacBookPro.local>
2020-07-22 15:09:20 -0600

    added security features

--- datingapp.api/Controllers/ValuesController.cs
+++ datingapp.api/Controllers/ValuesController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using datingapp.api.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -27,7 +29,7 @@ namespace DatingApp.API.Controllers
             return Ok(values);
         }
 
-        // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
@@ -42,7 +44,7 @@ namespace DatingApp.API.Controllers
         {
         }
 
-        // PUT api/values/5
+        
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
