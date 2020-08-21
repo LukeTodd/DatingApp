3448cc9082dc0b4ab3c50dbe39150769539610d8
Luke Todd <luketodd@Luke-Todd--luketoddhappyhippoherbalscom--MacBookPro.local>
2020-08-21 15:00:00 -0600

    Added Member Cards and Route Resolvers

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -4,12 +4,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 
-const HttpOptions = {
-  headers: new HttpHeaders({
-    Authorization: 'Bearer ' + localStorage.getItem('token')
-  })
-};
-
 @Injectable({
   providedIn: 'root'
 })
@@ -19,11 +13,10 @@ export class UserService {
 constructor(private http: HttpClient) {}
 
 getUsers(): Observable<User[]> {
-  return this.http.get<User[]>(this.baseUrl + 'users', HttpOptions);
+  return this.http.get<User[]>(this.baseUrl + 'users');
 }
 
 getUser(id): Observable<User> {
-  return this.http.get<User>(this.baseUrl + 'users/' + id, HttpOptions);
-}
-
+  return this.http.get<User>(this.baseUrl + 'users/' + id);
+ }
 }
